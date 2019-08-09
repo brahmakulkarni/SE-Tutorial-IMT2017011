862c0b17127ac32700a0b29c3c61cdcc06634de3
brahmakulkarni <brahma.kulkarni@iiitb.org]>
2019-08-09 16:27:23 +0530

    added file to subbranch

--- caladd.java
+++ caladd.java
@@ -9,7 +9,8 @@ public class caladd {
         do {
             System.out.print("1. Add Two Numbers\n");
             System.out.print("2. Multiply Two Numbers\n");
-            System.out.print("3. Exit\n\n");
+            System.out.print("3. Divide Two Numbers\n");
+            System.out.print("4. Exit\n\n");
             System.out.print("Enter your Choice: ");
             choice = scan.next().charAt(0);
             switch(choice) {
@@ -27,13 +28,20 @@ public class caladd {
                           System.out.print("Result = " + res);
                           break;
 
-                case '3': System.exit(0);
+                case '3': System.out.print("Enter Two Numbers: ");
+                          a = scan.nextFloat();
+                          b = scan.nextFloat();
+                          res = a/b;
+                          System.out.print("Result = " + res);
+                          break;
+
+                case '4': System.exit(0);
                           break;
                           
                 default: System.out.print("INVALID CHOICE!");
             }
             System.out.print("\n------------------");
         }
-        while (choice != 2);
+        while (choice != '2');
     }
 }
